2e326b8eed134e1d1ed64dc58da51481c34d79d4
Krapaud <krapaud@example.com>
2025-09-01 12:05:12 +0200

    Réorganiser le script pour vérifier les arguments avant de les afficher et ajouter un message lorsque aucun argument n'est fourni.

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""Script qui affiche tous les arguments passés en ligne de commande."""
 import sys
 
-# Correction: commencer à partir de l'index 1 pour exclure le nom du fichier Python
-for i in range(1, len(sys.argv)):
-    print(sys.argv[i])
+# Vérifier s'il y a des arguments à afficher
+if len(sys.argv) > 1:
+    # Commencer à partir de l'index 1 pour exclure le nom du fichier
+    for i in range(1, len(sys.argv)):
+        print(sys.argv[i])
+else:
+    print("Aucun argument fourni.")
